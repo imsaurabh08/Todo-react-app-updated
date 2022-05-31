89096523f8e4e79a0bd7052efc68550d3b50144b
imsaurabh08 <103378265+imsaurabh08@users.noreply.github.com>
2022-06-01 01:17:25 +0530

    make it better

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 
 const mongoose=require('mongoose');
 
-const mongoURI =process.env.DATABASE;
+const mongoURI ="mongodb+srv://saurabh:shradha08@cluster0.n18ihex.mongodb.net/?retryWrites=true&w=majority";
 
 console.log(mongoURI)
 const connectToMongo=()=>{
