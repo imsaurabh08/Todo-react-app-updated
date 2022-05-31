c83098ebb2d8d6bca521612329a792821123f322
imsaurabh08 <103378265+imsaurabh08@users.noreply.github.com>
2022-06-01 01:54:46 +0530

    make it better

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
 
-if(process.env.NODE_ENV=="production")
+if(process.env.NODE_ENV==="production")
 {
   app.use(express.static('client/build'))
   app.get("/", (req, res) => {
