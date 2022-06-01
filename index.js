e1fbd953527853d7fc88ab6f3b8accef6e4e6715
imsaurabh08 <103378265+imsaurabh08@users.noreply.github.com>
2022-06-01 16:07:26 +0530

    make it better

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname,  "client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname,  './client/build', "index.html"));
 });
 if(process.env.NODE_ENV==="production")
 {
