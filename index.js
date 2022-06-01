8997b9116610c6a9c8125c2c939c4f071c712fd0
imsaurabh08 <103378265+imsaurabh08@users.noreply.github.com>
2022-06-01 16:03:47 +0530

    make it better

--- index.js
+++ index.js
@@ -10,20 +10,18 @@ connectToMongo()
 
 const app = express()
 const port =  process.env.PORT || 5000;
-// app.use(express.static(path.resolve(__dirname,"../build")))
+
 app.use(express.json());
 app.use(cors())
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
-// app.get("/", (req, res) => {
-//   res.json("hurray");
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname,  "client/build", "index.html"));
+});
 if(process.env.NODE_ENV==="production")
 {
   app.use(express.static('client/build'))
-  // app.get("/", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname,  "client/build", "index.html"));
-  // });
+ 
 }
 app.listen(port, () => {
   console.log(`Example app listening at port http://localhost:${port}`)
